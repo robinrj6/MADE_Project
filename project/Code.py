0115fc6e221017c73e199b5f675f6156047cd604
Robin Jose Raju <robinrj6@gmail.com>
2024-06-10 16:37:24 +0200

    update: Code modified to more structured way, testcases added

--- project/Code.py
+++ project/Code.py
@@ -2,46 +2,71 @@ import py7zr
 import requests
 import pandas as pd
 
-df1 = pd.read_csv(
+from Tests import run_tests
+
+# Function to read data from a source
+def getData(url, sep):
+    df = pd.read_csv(url, sep=sep)
+    return df
+
+
+# Function to interpret the data
+def interpretData(df, column_names):
+    df = df[column_names]
+    return df
+
+
+# Function to load data into a database
+def load(table_name, sheet):
+    sheet.to_sql(table_name, 'sqlite:///../data/Store.sqlite',
+                 if_exists='replace', index=False)
+
+
+# PRIMAP data
+
+df1 = getData(
     'https://zenodo.org/records/4723476/files/Guetschow-et-al-2021-PRIMAP-crf_2021-v1.csv?download=1',
-    sep=','
-)
+    sep=',')
 
-df1 = df1[["area (ISO3)", 
-           "entity", 
-           "unit", 
-           "category (IPCC2006)", 
-           "1996", 
-           "1997", 
-           "1998", 
-           "1999",
-           "2000",
-           "2001",
-           "2002",
-           "2003",
-           "2004",
-           "2005",
-           "2006",
-           "2007",
-           "2008",
-           "2009",
-           "2010",
-           "2011",
-           "2012",
-           "2013",
-           "2014",
-           "2015",
-           "2016",
-           "2017",
-           "2018",
-           "2019",]]
+df1 = interpretData(df1, ["area (ISO3)",
+                          "entity",
+                          "unit",
+                          "category (IPCC2006)",
+                          "1996",
+                          "1997",
+                          "1998",
+                          "1999",
+                          "2000",
+                          "2001",
+                          "2002",
+                          "2003",
+                          "2004",
+                          "2005",
+                          "2006",
+                          "2007",
+                          "2008",
+                          "2009",
+                          "2010",
+                          "2011",
+                          "2012",
+                          "2013",
+                          "2014",
+                          "2015",
+                          "2016",
+                          "2017",
+                          "2018",
+                          "2019",])
 
-df1=df1.dropna()
+
+# Cleaning the data
+df1 = df1.dropna()
 df_zero_category = df1.loc[df1['category (IPCC2006)'] == '0']
 
-df_zero_category.to_sql('PRIMAP', 'sqlite:///../data/PRIMAP.sqlite',
-           if_exists='replace', index=False)
 
+load('PRIMAP', df_zero_category)
+
+
+# Disease Outbreaks data
 
 url = "https://figshare.com/ndownloader/files/37887318"
 response = requests.get(url)
@@ -53,18 +78,19 @@ with open('file.7z', 'wb') as f:
 with py7zr.SevenZipFile('file.7z', mode='r') as z:
     z.extractall()
 
-df2 = pd.read_csv('./DiseaseOutbreaks/Outbreaks.csv', sep=',')
+df2 = getData('./DiseaseOutbreaks/Outbreaks.csv', sep=',')
 
-df2 = df2[["Country", 
-           "iso3", 
-           "Year", 
-           "icd10n",
-           "icd104n", 
-           "icd11l1", 
-           "icd11l3", 
-           "Disease"]]
+df2 = interpretData(df2, ["Country",
+                          "iso3",
+                          "Year",
+                          "icd10n",
+                          "icd104n",
+                          "icd11l1",
+                          "icd11l3",
+                          "Disease"])
 
 df2_before_2020 = df2.loc[df2['Year'] < 2020]
 
-df2_before_2020.to_sql('Diseases', 'sqlite:///../data/diseases.sqlite',
-           if_exists='replace', index=False)
+
+load('Diseases', df2_before_2020)
+run_tests()
---
+++ project/Tests.py
@@ -0,0 +1,33 @@
+import os
+import sqlite3
+
+
+# test functions
+def test_store_exists():
+    if os.path.exists('../data/Store.sqlite'):
+        print('Store found Successfully')
+        return True
+    else:
+        print('Store not found')
+        return False
+    
+# test table exists or not
+def test_table_exists(name):
+    conn = sqlite3.connect('../data/Store.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM {name};")
+    result = cursor.fetchone()
+
+    conn.close()
+
+    if result:
+        print(f"Table {name} exists.")
+    else:
+        print(f"Table {name} does not exist.")
+    
+# run tests
+def run_tests():
+    if test_store_exists():
+        test_table_exists('PRIMAP')
+        test_table_exists('Diseases')
