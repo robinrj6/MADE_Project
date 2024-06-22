ce7557371918625c4bd0b528ccc414e2d231ce35
Robin Jose Raju <robinrj6@gmail.com>
2024-06-22 14:07:52 +0200

    test: testing the testfile

--- project/Tests.py
+++ project/Tests.py
@@ -6,7 +6,7 @@ import sys
 
 
 def test_store_exists():
-    if os.path.exists('./data/Store.sqlite'):
+    if os.path.exists('../data/Store.sqlite'):
         print('Store found Successfully')
         return True
     else:
@@ -18,7 +18,7 @@ def test_store_exists():
 
 
 def test_table_exists(name):
-    conn = sqlite3.connect('./data/Store.sqlite')
+    conn = sqlite3.connect('../data/Store.sqlite')
     cursor = conn.cursor()
 
     try:
