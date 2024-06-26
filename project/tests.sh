272c48fbaaeb4ba5fffaeb35ee72d6b8206b84d3
Robin Jose Raju <robinrj6@gmail.com>
2024-06-26 19:21:05 +0200

    update:  test.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-pip install pandas
-pip install py7zr
-pip install requests
-pip install sqlalchemy
-python project/Code.py
+# pip install pandas
+# pip install py7zr
+# pip install requests
+# pip install sqlalchemy
+# python project/Code.py
+# An issue occuring with the environment where the code is running, the code is not able to read the store.sqlite from the path. The code is working fine in the local environment.
+# This in the end leads to the failure of the test cases as the test cases look for the data in the store.sqlite file but the file is not found.
+# The workflow executes the tests on every push to the main branch and an output is generated which shows the test cases are failing.
 python project/Tests.py
